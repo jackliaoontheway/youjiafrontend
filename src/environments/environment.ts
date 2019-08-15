6ca7654e9946f02000bdab821a65d757c8ea70d1
Jack <ldhontheway@live.com>
2019-08-15 17:41:00 +0800

    修改url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
     production: false,
     hmr       : false,
     enableLog : true,
-    baseUrl   : 'http://localhost:8080/dragoncargo'
+    baseUrl   : 'http://localhost:8080/youjiabackend'
 };
 
 /*
